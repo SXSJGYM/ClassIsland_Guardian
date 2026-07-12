5c799dc03432973554e28acfeac25f4037bba787
GYM <gym753357@qq.com>
2026-07-12 23:25:42 +0800

    refactor: 优化文件过滤器数据结构和回调函数，增强拦截逻辑（能用了，但是拦截不了移动文件（）

--- drivers/drivers/file/file.c
+++ drivers/drivers/file/file.c
@@ -4,11 +4,11 @@
 
 #pragma prefast(disable:__WARNING_ENCODE_MEMBER_FUNCTION_POINTER, "Not valid for kernel mode drivers")
 
-typedef struct _CIG_FILTER_DATA {
+typedef struct _CIGFile_FILTER_DATA {
 
     PFLT_FILTER FilterHandle;
 
-} CIGFile_FILTER_DATA, * PNULL_FILTER_DATA;
+} CIGFile_FILTER_DATA, * PCIGFILE_FILTER_DATA;
 
 DRIVER_INITIALIZE DriverEntry;
 NTSTATUS
@@ -28,7 +28,7 @@ CIGFileQueryTeardown(
     _In_ FLT_INSTANCE_QUERY_TEARDOWN_FLAGS Flags
 );
 
-CIGFile_FILTER_DATA NullFilterData;
+CIGFile_FILTER_DATA CIGFileFilterData;
 
 #ifdef ALLOC_PRAGMA
 #pragma alloc_text(INIT, DriverEntry)
@@ -52,13 +52,14 @@ CIGPreRead(
     _Out_ PVOID* CompletionContext
 );
 
+
 // 定义操作回调表-全部拦截
 CONST FLT_OPERATION_REGISTRATION Callbacks[] = {
     { IRP_MJ_CREATE, 0, CIGPreWrite, NULL },  // 打开/创建
     { IRP_MJ_READ, 0, CIGPreRead, NULL },  // 读取
     { IRP_MJ_WRITE, 0, CIGPreWrite, NULL },  // 写入
     { IRP_MJ_SET_INFORMATION, 0, CIGPreWrite, NULL },  // 删除/重命名/改属性
-    { IRP_MJ_DIRECTORY_CONTROL,0, CIGPreRead, NULL },  // 遍历目录（列举文件）
+    { IRP_MJ_DIRECTORY_CONTROL,0,  CIGPreRead, NULL },  // 遍历目录（列举文件）
     { IRP_MJ_OPERATION_END }
 };
 
@@ -96,15 +97,15 @@ DriverEntry(
 
     status = FltRegisterFilter(DriverObject,
         &FilterRegistration,
-        &NullFilterData.FilterHandle);
+        &CIGFileFilterData.FilterHandle);
 
     FLT_ASSERT(NT_SUCCESS(status));
 
     if (NT_SUCCESS(status)) {
-        status = FltStartFiltering(NullFilterData.FilterHandle);
+        status = FltStartFiltering(CIGFileFilterData.FilterHandle);
 
         if (!NT_SUCCESS(status)) {
-            FltUnregisterFilter(NullFilterData.FilterHandle);
+            FltUnregisterFilter(CIGFileFilterData.FilterHandle);
         }
     }
     DbgPrint("[CIG] Driver initialized and filtering started\n");
@@ -120,7 +121,7 @@ CIGFileUnload(
 
     PAGED_CODE();
 
-    FltUnregisterFilter(NullFilterData.FilterHandle);
+    FltUnregisterFilter(CIGFileFilterData.FilterHandle);
 
     return STATUS_SUCCESS;
 }
@@ -152,7 +153,71 @@ CIGPreWrite(
     UNREFERENCED_PARAMETER(FltObjects);
     UNREFERENCED_PARAMETER(CompletionContext);
 
-    // 先不写，直接返回成功
+     // 快速核验
+    // 高IRQL下直接放行
+    if (KeGetCurrentIrql() > PASSIVE_LEVEL) {
+        return FLT_PREOP_SUCCESS_NO_CALLBACK;
+    }
+
+    // 快速核验是否为白名单进程
+    PUNICODE_STRING processPath = NULL;
+    if (NT_SUCCESS(SeLocateProcessImageName(PsGetCurrentProcess(), &processPath)) && processPath != NULL) {
+        UNICODE_STRING guardianExePath = { 0 };
+        RtlInitUnicodeString(&guardianExePath, L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\GUARDIAN.EXE");
+        UNICODE_STRING configExePath = { 0 };
+        RtlInitUnicodeString(&configExePath, L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\CONFIG.EXE");
+        __try {
+            if (FsRtlIsNameInExpression(&guardianExePath, processPath, TRUE, NULL) || FsRtlIsNameInExpression(&configExePath, processPath, TRUE, NULL)) {
+                DbgPrint("[CIG] PassApp: %wZ\n", processPath);
+                ExFreePool(processPath);
+                return FLT_PREOP_SUCCESS_NO_CALLBACK;
+            }
+        }
+        __except (EXCEPTION_EXECUTE_HANDLER) {
+            ;
+        }
+        ExFreePool(processPath);
+    }
+
+    // 特例处理
+    // GuardianRecovery目录
+    UNICODE_STRING guardianRecoveryPath = { 0 };
+    RtlInitUnicodeString(&guardianRecoveryPath, L"\\DEVICE\\HARDDISKVOLUME*\\GUARDIANRECOVERY*");
+    // Guardian目录
+    UNICODE_STRING guardianPath = { 0 };
+    RtlInitUnicodeString(&guardianPath, L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN*");
+
+    UCHAR majorFunction = Data->Iopb->MajorFunction;
+
+    // 判断是否为打开
+    if (majorFunction == IRP_MJ_CREATE) {
+        ULONG createDisposition = (Data->Iopb->Parameters.Create.Options >> 24) & 0xFF;
+        if (createDisposition == FILE_OPEN) {
+            return FLT_PREOP_SUCCESS_NO_CALLBACK;
+        }
+    }
+
+    // 通用拦截
+    // 获取被操作目录
+    PFLT_FILE_NAME_INFORMATION fileNameInfo = NULL;
+    if (NT_SUCCESS(FltGetFileNameInformation(
+        Data,
+        FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT,
+        &fileNameInfo
+    ))) {
+        if (NT_SUCCESS(FltParseFileNameInformation(fileNameInfo))) {
+            // 拦截逻辑
+            if (FsRtlIsNameInExpression(&guardianRecoveryPath, &fileNameInfo->Name, TRUE, NULL) || FsRtlIsNameInExpression(&guardianPath, &fileNameInfo->Name, TRUE, NULL)) {
+                DbgPrint("[CIG] ParentDir: %wZ\n", &fileNameInfo->Name);
+                DbgPrint("[CIG] FLT_PREOP_COMPLETE\n\n");
+                Data->IoStatus.Status = STATUS_ACCESS_DENIED;
+                Data->IoStatus.Information = 0;
+                FltReleaseFileNameInformation(fileNameInfo);
+                return FLT_PREOP_COMPLETE;
+            }
+        }
+        FltReleaseFileNameInformation(fileNameInfo);
+    }
     return FLT_PREOP_SUCCESS_NO_CALLBACK;
 }
 
@@ -169,24 +234,34 @@ CIGPreRead(
     UNREFERENCED_PARAMETER(FltObjects);
     UNREFERENCED_PARAMETER(CompletionContext);
 
-    DbgPrint("[CIG] PreRead entered\n");
+    // 高IRQL下直接放行
+    if (KeGetCurrentIrql() > PASSIVE_LEVEL) {
+        return FLT_PREOP_SUCCESS_NO_CALLBACK;
+    }
 
     // 快速核验是否为白名单进程
-    if (KeGetCurrentIrql() <= PASSIVE_LEVEL) {
-        PUNICODE_STRING processPath = NULL;
-        if (NT_SUCCESS(SeLocateProcessImageName(PsGetCurrentProcess(), &processPath)) && processPath != NULL) {
-            //DbgPrint("Process Image Path: %wZ\n",processPath);
-            ExFreePool(processPath);
+    PUNICODE_STRING processPath = NULL;
+    if (NT_SUCCESS(SeLocateProcessImageName(PsGetCurrentProcess(), &processPath)) && processPath != NULL) {
+        UNICODE_STRING guardianExePath = { 0 };
+        RtlInitUnicodeString(&guardianExePath, L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\GUARDIAN.EXE");
+        UNICODE_STRING configExePath = { 0 };
+        RtlInitUnicodeString(&configExePath, L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\CONFIG.EXE");
+        __try {
+            if (FsRtlIsNameInExpression(&guardianExePath, processPath, TRUE, NULL) || FsRtlIsNameInExpression(&configExePath, processPath, TRUE, NULL)) {
+                DbgPrint("[CIG] PassApp: %wZ\n", processPath);
+                ExFreePool(processPath);
+                return FLT_PREOP_SUCCESS_NO_CALLBACK;
+            }
         }
-    }
-    else {
-        // 不安全：放弃获取进程路径，直接放行
-        //DbgPrint("[CIG] IRQL too high, skipping process path check\n");
+        __except (EXCEPTION_EXECUTE_HANDLER){
+            ;
+        }
+        ExFreePool(processPath);
     }
 
     // GuardianRecovery目录
     UNICODE_STRING guardianRecoveryPath = { 0 };
-    RtlInitUnicodeString(&guardianRecoveryPath, L"\\GuardianRecovery");
+    RtlInitUnicodeString(&guardianRecoveryPath, L"\\DEVICE\\HARDDISKVOLUME*\\GUARDIANRECOVERY*");
 
     // 获取被操作目录
     PFLT_FILE_NAME_INFORMATION fileNameInfo = NULL;
@@ -195,12 +270,11 @@ CIGPreRead(
         FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT,
         &fileNameInfo
     ))) {
-        DbgPrint("[CIG] FltGetFileNameInformation entered\n");
         if (NT_SUCCESS(FltParseFileNameInformation(fileNameInfo))) {
             // 拦截逻辑
-            DbgPrint("[CIG] Read Name: %wZ\n", &fileNameInfo->Name);
-            if (RtlPrefixUnicodeString(&guardianRecoveryPath, &fileNameInfo -> Name, TRUE)) {
-                DbgPrint("[CIG] FLT_PREOP_COMPLETE\n");
+            if (FsRtlIsNameInExpression(&guardianRecoveryPath, &fileNameInfo->Name, TRUE, NULL)) {
+                DbgPrint("[CIG] ParentDir: %wZ\n", &fileNameInfo->Name);
+                DbgPrint("[CIG] FLT_PREOP_COMPLETE\n\n");
                 Data->IoStatus.Status = STATUS_ACCESS_DENIED;
                 Data->IoStatus.Information = 0;
                 FltReleaseFileNameInformation(fileNameInfo);
