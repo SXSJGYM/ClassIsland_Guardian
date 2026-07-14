78d1fa0c1af5ff11131bb72b6d770284fb7fb021
GYM <gym753357@qq.com>
2026-07-14 22:14:01 +0800

    feat: 添加白名单进程验证和路径保护，优化文件操作拦截逻辑（file.sys通过7小时Driver Verifier压力测试）

--- drivers/drivers/file/file.c
+++ drivers/drivers/file/file.c
@@ -4,6 +4,32 @@
 
 #pragma prefast(disable:__WARNING_ENCODE_MEMBER_FUNCTION_POINTER, "Not valid for kernel mode drivers")
 
+#define _WIN32_WINNT 0x0A00
+
+// 定义保护与白名单路径
+#pragma data_seg("NONPAGED")
+// 白名单
+UNICODE_STRING g_guardianExePath = { 0 };
+UNICODE_STRING g_configExePath = { 0 };
+// 保护
+UNICODE_STRING g_guardianRecoveryPath = { 0 };
+UNICODE_STRING g_guardianPath = { 0 };
+#pragma data_seg()
+
+// 手动定义 FileRenameInformationEx 结构体
+#if !defined(FILE_RENAME_INFORMATION_EX_DEFINED)
+#define FILE_RENAME_INFORMATION_EX_DEFINED
+
+typedef struct _FILE_RENAME_INFORMATION_EX {
+    BOOLEAN ReplaceIfExists;
+    HANDLE  RootDirectory;
+    ULONG   FileNameLength;
+    ULONG   Flags;
+    WCHAR   FileName[1];
+} FILE_RENAME_INFORMATION_EX, * PFILE_RENAME_INFORMATION_EX;
+
+#endif
+
 typedef struct _CIGFile_FILTER_DATA {
 
     PFLT_FILTER FilterHandle;
@@ -95,6 +121,57 @@ DriverEntry(
 
     UNREFERENCED_PARAMETER(RegistryPath);
 
+    // 分配非分页内存并复制字符串
+    PWCHAR buffer;
+
+    // guardianRecoveryPath
+    buffer = (PWCHAR)ExAllocatePool2(POOL_FLAG_NON_PAGED,
+        sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\GUARDIANRECOVERY*"),
+        'CIGP');
+    if (buffer) {
+        RtlCopyMemory(buffer, L"\\DEVICE\\HARDDISKVOLUME*\\GUARDIANRECOVERY*",
+            sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\GUARDIANRECOVERY*"));
+        g_guardianRecoveryPath.Buffer = buffer;
+        g_guardianRecoveryPath.Length = (USHORT)(sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\GUARDIANRECOVERY*") - sizeof(WCHAR));
+        g_guardianRecoveryPath.MaximumLength = (USHORT)sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\GUARDIANRECOVERY*");
+    }
+
+    // guardianPath
+    buffer = (PWCHAR)ExAllocatePool2(POOL_FLAG_NON_PAGED,
+        sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN*"),
+        'CIGP');
+    if (buffer) {
+        RtlCopyMemory(buffer, L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN*",
+            sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN*"));
+        g_guardianPath.Buffer = buffer;
+        g_guardianPath.Length = (USHORT)(sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN*") - sizeof(WCHAR));
+        g_guardianPath.MaximumLength = (USHORT)sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN*");
+    }
+
+    // guardianExePath
+    buffer = (PWCHAR)ExAllocatePool2(POOL_FLAG_NON_PAGED,
+        sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\GUARDIAN.EXE"),
+        'CIGP');
+    if (buffer) {
+        RtlCopyMemory(buffer, L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\GUARDIAN.EXE",
+            sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\GUARDIAN.EXE"));
+        g_guardianExePath.Buffer = buffer;
+        g_guardianExePath.Length = (USHORT)(sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\GUARDIAN.EXE") - sizeof(WCHAR));
+        g_guardianExePath.MaximumLength = (USHORT)sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\GUARDIAN.EXE");
+    }
+
+    // configExePath
+    buffer = (PWCHAR)ExAllocatePool2(POOL_FLAG_NON_PAGED,
+        sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\CONFIG.EXE"),
+        'CIGP');
+    if (buffer) {
+        RtlCopyMemory(buffer, L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\CONFIG.EXE",
+            sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\CONFIG.EXE"));
+        g_configExePath.Buffer = buffer;
+        g_configExePath.Length = (USHORT)(sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\CONFIG.EXE") - sizeof(WCHAR));
+        g_configExePath.MaximumLength = (USHORT)sizeof(L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\CONFIG.EXE");
+    }
+
     status = FltRegisterFilter(DriverObject,
         &FilterRegistration,
         &CIGFileFilterData.FilterHandle);
@@ -108,7 +185,6 @@ DriverEntry(
             FltUnregisterFilter(CIGFileFilterData.FilterHandle);
         }
     }
-    DbgPrint("[CIG] Driver initialized and filtering started\n");
     return status;
 }
 
@@ -122,6 +198,11 @@ CIGFileUnload(
     PAGED_CODE();
 
     FltUnregisterFilter(CIGFileFilterData.FilterHandle);
+
+    if (g_guardianRecoveryPath.Buffer) ExFreePool(g_guardianRecoveryPath.Buffer);
+    if (g_guardianPath.Buffer)          ExFreePool(g_guardianPath.Buffer);
+    if (g_guardianExePath.Buffer)       ExFreePool(g_guardianExePath.Buffer);
+    if (g_configExePath.Buffer)         ExFreePool(g_configExePath.Buffer);
 
     return STATUS_SUCCESS;
 }
@@ -140,7 +221,29 @@ CIGFileQueryTeardown(
     return STATUS_SUCCESS;
 }
 
-// 写入拦截策略
+// 白名单放行策略
+BOOLEAN IsProcessTrusted(VOID) {
+    PUNICODE_STRING processPath = NULL;
+    if (NT_SUCCESS(SeLocateProcessImageName(PsGetCurrentProcess(), &processPath)) && processPath != NULL) {
+        __try {
+            UNICODE_STRING saveProcessPath;
+            WCHAR saveProcessPath_buf[512];
+            RtlInitEmptyUnicodeString(&saveProcessPath, saveProcessPath_buf, 512 * sizeof(WCHAR));
+            RtlCopyUnicodeString(&saveProcessPath, processPath);
+            if (FsRtlIsNameInExpression(&g_guardianExePath, &saveProcessPath, TRUE, NULL) || FsRtlIsNameInExpression(&g_configExePath, &saveProcessPath, TRUE, NULL)) {
+                ExFreePool(processPath);
+                return TRUE;
+            }
+        }
+        __except (EXCEPTION_EXECUTE_HANDLER) {
+            ;
+        }
+        ExFreePool(processPath);
+    }
+    return FALSE;
+}
+
+// 写入拦截策略----------------------------------------------------------
 FLT_PREOP_CALLBACK_STATUS
 CIGPreWrite(
     _Inout_ PFLT_CALLBACK_DATA Data,
@@ -158,42 +261,73 @@ CIGPreWrite(
     if (KeGetCurrentIrql() > PASSIVE_LEVEL) {
         return FLT_PREOP_SUCCESS_NO_CALLBACK;
     }
-
-    // 快速核验是否为白名单进程
-    PUNICODE_STRING processPath = NULL;
-    if (NT_SUCCESS(SeLocateProcessImageName(PsGetCurrentProcess(), &processPath)) && processPath != NULL) {
-        UNICODE_STRING guardianExePath = { 0 };
-        RtlInitUnicodeString(&guardianExePath, L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\GUARDIAN.EXE");
-        UNICODE_STRING configExePath = { 0 };
-        RtlInitUnicodeString(&configExePath, L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\CONFIG.EXE");
-        __try {
-            if (FsRtlIsNameInExpression(&guardianExePath, processPath, TRUE, NULL) || FsRtlIsNameInExpression(&configExePath, processPath, TRUE, NULL)) {
-                DbgPrint("[CIG] PassApp: %wZ\n", processPath);
-                ExFreePool(processPath);
-                return FLT_PREOP_SUCCESS_NO_CALLBACK;
-            }
-        }
-        __except (EXCEPTION_EXECUTE_HANDLER) {
-            ;
-        }
-        ExFreePool(processPath);
+    // 白名单进程直接放行
+    if (IsProcessTrusted()) {
+        return FLT_PREOP_SUCCESS_NO_CALLBACK;
     }
 
-    // 特例处理
-    // GuardianRecovery目录
-    UNICODE_STRING guardianRecoveryPath = { 0 };
-    RtlInitUnicodeString(&guardianRecoveryPath, L"\\DEVICE\\HARDDISKVOLUME*\\GUARDIANRECOVERY*");
-    // Guardian目录
-    UNICODE_STRING guardianPath = { 0 };
-    RtlInitUnicodeString(&guardianPath, L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN*");
-
     UCHAR majorFunction = Data->Iopb->MajorFunction;
-
     // 判断是否为打开
     if (majorFunction == IRP_MJ_CREATE) {
         ULONG createDisposition = (Data->Iopb->Parameters.Create.Options >> 24) & 0xFF;
         if (createDisposition == FILE_OPEN) {
             return FLT_PREOP_SUCCESS_NO_CALLBACK;
+        }
+    }
+    // 判断是否为移动
+    if (majorFunction == IRP_MJ_SET_INFORMATION) {
+        FILE_INFORMATION_CLASS infoClass = Data->Iopb->Parameters.SetFileInformation.FileInformationClass;
+        if (infoClass == FileRenameInformation || infoClass == FileRenameInformationEx) {
+            UNICODE_STRING targetFileName = { 0 };
+            HANDLE rootDir = NULL;
+            PVOID infoBuffer = Data->Iopb->Parameters.SetFileInformation.InfoBuffer;
+            if (infoClass == FileRenameInformation) {
+                PFILE_RENAME_INFORMATION pInfo = (PFILE_RENAME_INFORMATION)infoBuffer;
+                rootDir = pInfo->RootDirectory;
+                targetFileName.Buffer = pInfo->FileName;
+                targetFileName.Length = (USHORT) pInfo->FileNameLength;
+                targetFileName.MaximumLength = (USHORT) pInfo->FileNameLength;
+            }
+            else {
+                PFILE_RENAME_INFORMATION_EX pInfoEx = (PFILE_RENAME_INFORMATION_EX)infoBuffer;
+                rootDir = pInfoEx->RootDirectory;
+                targetFileName.Buffer = pInfoEx->FileName;
+                targetFileName.Length = (USHORT) pInfoEx->FileNameLength;
+                targetFileName.MaximumLength = (USHORT) pInfoEx->FileNameLength;
+            }
+            PFLT_FILE_NAME_INFORMATION targetInfo = NULL;
+            NTSTATUS status = FltGetDestinationFileNameInformation(
+                FltObjects->Instance,
+                FltObjects->FileObject,
+                rootDir,
+                targetFileName.Buffer,
+                targetFileName.Length,
+                FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT,
+                &targetInfo
+            );
+            if (!NT_SUCCESS(status)) {
+                // 仅放行极少数卷卸载，其余一律拒绝
+                if (status == STATUS_FLT_DELETING_OBJECT || status == STATUS_VOLUME_DISMOUNTED) {
+                    return FLT_PREOP_SUCCESS_NO_CALLBACK;
+                }
+                else {
+                    Data->IoStatus.Status = STATUS_ACCESS_DENIED;
+                    Data->IoStatus.Information = 0;
+                    return FLT_PREOP_COMPLETE;
+                }
+            }
+            else {
+                FltParseFileNameInformation(targetInfo);
+                // 拦截逻辑
+                if (FsRtlIsNameInExpression(&g_guardianRecoveryPath, &targetInfo->Name, TRUE, NULL) ||
+                    FsRtlIsNameInExpression(&g_guardianPath, &targetInfo->Name, TRUE, NULL)) {
+                    Data->IoStatus.Status = STATUS_ACCESS_DENIED;
+                    Data->IoStatus.Information = 0;
+                    FltReleaseFileNameInformation(targetInfo);
+                    return FLT_PREOP_COMPLETE;
+                }
+                FltReleaseFileNameInformation(targetInfo);
+            }
         }
     }
 
@@ -205,23 +339,41 @@ CIGPreWrite(
         FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT,
         &fileNameInfo
     ))) {
-        if (NT_SUCCESS(FltParseFileNameInformation(fileNameInfo))) {
-            // 拦截逻辑
-            if (FsRtlIsNameInExpression(&guardianRecoveryPath, &fileNameInfo->Name, TRUE, NULL) || FsRtlIsNameInExpression(&guardianPath, &fileNameInfo->Name, TRUE, NULL)) {
-                DbgPrint("[CIG] ParentDir: %wZ\n", &fileNameInfo->Name);
-                DbgPrint("[CIG] FLT_PREOP_COMPLETE\n\n");
-                Data->IoStatus.Status = STATUS_ACCESS_DENIED;
-                Data->IoStatus.Information = 0;
-                FltReleaseFileNameInformation(fileNameInfo);
-                return FLT_PREOP_COMPLETE;
+
+        NTSTATUS status = FltParseFileNameInformation(fileNameInfo);
+        if (NT_SUCCESS(status)) {
+            UNICODE_STRING saveFileNameInfo;
+            WCHAR savefileNameInfo_buf[512];
+            RtlInitEmptyUnicodeString(&saveFileNameInfo, savefileNameInfo_buf, 512 * sizeof(WCHAR));
+            RtlCopyUnicodeString(&saveFileNameInfo, &fileNameInfo->Name);
+
+            __try {
+                // 拦截逻辑
+                if (FsRtlIsNameInExpression(&g_guardianRecoveryPath, &saveFileNameInfo, TRUE, NULL) || FsRtlIsNameInExpression(&g_guardianPath, &saveFileNameInfo, TRUE, NULL)) {
+                    Data->IoStatus.Status = STATUS_ACCESS_DENIED;
+                    Data->IoStatus.Information = 0;
+                    FltReleaseFileNameInformation(fileNameInfo);
+                    return FLT_PREOP_COMPLETE;
+                }
             }
+            __except (EXCEPTION_EXECUTE_HANDLER) {
+                ;
+            }
+        }
+        else {
+            if (status == STATUS_FLT_DELETING_OBJECT || status == STATUS_VOLUME_DISMOUNTED) {
+                return FLT_PREOP_SUCCESS_NO_CALLBACK;
+            }
+            Data->IoStatus.Status = STATUS_ACCESS_DENIED;
+            Data->IoStatus.Information = 0;
+            return FLT_PREOP_COMPLETE;
         }
         FltReleaseFileNameInformation(fileNameInfo);
     }
     return FLT_PREOP_SUCCESS_NO_CALLBACK;
 }
 
-// 读取拦截策略
+// 读取拦截策略----------------------------------------------------------
 FLT_PREOP_CALLBACK_STATUS
 CIGPreRead(
     _Inout_ PFLT_CALLBACK_DATA Data,
@@ -234,34 +386,15 @@ CIGPreRead(
     UNREFERENCED_PARAMETER(FltObjects);
     UNREFERENCED_PARAMETER(CompletionContext);
 
+    // 快速核验
     // 高IRQL下直接放行
     if (KeGetCurrentIrql() > PASSIVE_LEVEL) {
         return FLT_PREOP_SUCCESS_NO_CALLBACK;
     }
-
-    // 快速核验是否为白名单进程
-    PUNICODE_STRING processPath = NULL;
-    if (NT_SUCCESS(SeLocateProcessImageName(PsGetCurrentProcess(), &processPath)) && processPath != NULL) {
-        UNICODE_STRING guardianExePath = { 0 };
-        RtlInitUnicodeString(&guardianExePath, L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\GUARDIAN.EXE");
-        UNICODE_STRING configExePath = { 0 };
-        RtlInitUnicodeString(&configExePath, L"\\DEVICE\\HARDDISKVOLUME*\\PROGRAM FILES\\GUARDIAN\\CONFIG.EXE");
-        __try {
-            if (FsRtlIsNameInExpression(&guardianExePath, processPath, TRUE, NULL) || FsRtlIsNameInExpression(&configExePath, processPath, TRUE, NULL)) {
-                DbgPrint("[CIG] PassApp: %wZ\n", processPath);
-                ExFreePool(processPath);
-                return FLT_PREOP_SUCCESS_NO_CALLBACK;
-            }
-        }
-        __except (EXCEPTION_EXECUTE_HANDLER){
-            ;
-        }
-        ExFreePool(processPath);
+    // 白名单进程直接放行
+    if (IsProcessTrusted()) {
+        return FLT_PREOP_SUCCESS_NO_CALLBACK;
     }
-
-    // GuardianRecovery目录
-    UNICODE_STRING guardianRecoveryPath = { 0 };
-    RtlInitUnicodeString(&guardianRecoveryPath, L"\\DEVICE\\HARDDISKVOLUME*\\GUARDIANRECOVERY*");
 
     // 获取被操作目录
     PFLT_FILE_NAME_INFORMATION fileNameInfo = NULL;
@@ -270,16 +403,35 @@ CIGPreRead(
         FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT,
         &fileNameInfo
     ))) {
-        if (NT_SUCCESS(FltParseFileNameInformation(fileNameInfo))) {
+        NTSTATUS status = FltParseFileNameInformation(fileNameInfo);
+        if (NT_SUCCESS(status)) {
+            UNICODE_STRING saveFileNameInfo;
+            WCHAR savefileNameInfo_buf[512];
+            RtlInitEmptyUnicodeString(&saveFileNameInfo, savefileNameInfo_buf, 512 * sizeof(WCHAR));
+            RtlCopyUnicodeString(&saveFileNameInfo, &fileNameInfo->Name);
+
             // 拦截逻辑
-            if (FsRtlIsNameInExpression(&guardianRecoveryPath, &fileNameInfo->Name, TRUE, NULL)) {
-                DbgPrint("[CIG] ParentDir: %wZ\n", &fileNameInfo->Name);
-                DbgPrint("[CIG] FLT_PREOP_COMPLETE\n\n");
-                Data->IoStatus.Status = STATUS_ACCESS_DENIED;
-                Data->IoStatus.Information = 0;
-                FltReleaseFileNameInformation(fileNameInfo);
-                return FLT_PREOP_COMPLETE;
+            __try {
+                if (FsRtlIsNameInExpression(&g_guardianRecoveryPath, &saveFileNameInfo, TRUE, NULL)) {
+                    DbgPrint("[CIG] ParentDir: %wZ\n", &fileNameInfo->Name);
+                    DbgPrint("[CIG] FLT_PREOP_COMPLETE\n\n");
+                    Data->IoStatus.Status = STATUS_ACCESS_DENIED;
+                    Data->IoStatus.Information = 0;
+                    FltReleaseFileNameInformation(fileNameInfo);
+                    return FLT_PREOP_COMPLETE;
+                }
             }
+            __except (EXCEPTION_EXECUTE_HANDLER) {
+                    ;
+                }
+        }
+        else {
+            if (status == STATUS_FLT_DELETING_OBJECT || status == STATUS_VOLUME_DISMOUNTED) {
+                return FLT_PREOP_SUCCESS_NO_CALLBACK;
+            }
+            Data->IoStatus.Status = STATUS_ACCESS_DENIED;
+            Data->IoStatus.Information = 0;
+            return FLT_PREOP_COMPLETE;
         }
         FltReleaseFileNameInformation(fileNameInfo);
     }
