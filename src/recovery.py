9e0fda30c8cc6c544a261b93d54d24695f65893f
GYM <gym753357@qq.com>
2026-07-22 22:44:11 +0800

    feat: 添加 recovery.py 日志迁移功能，添加为 check.exe 添加开机自启项功能

--- src/recovery.py
+++ src/recovery.py
@@ -21,18 +21,18 @@ def find_guardianrecovery_path():
 
 
 def main():
+    # 获取 GuardianRecovery 路径
     guardianrecovery_path = find_guardianrecovery_path()
-    drive, _ = os.path.splitdrive(guardianrecovery_path)
-    guardian_path = os.path.join(drive, "Program Files", "Guardian")
-
-    Log.logfile = os.path.join(guardianrecovery_path,'guardian.log')
-
+    Log.logfile = os.path.join(guardianrecovery_path,'recovery.log')
     clear()
     Log.info('正在初始化预启动修复恢复环境...')
     if(not guardianrecovery_path):
         Log.error('未找到可用的恢复路径。程序将会退出。')
         sys.exit(0)
     Log.info(f'寻找到了可用的恢复目录：{guardianrecovery_path}')
+
+    drive, _ = os.path.splitdrive(guardianrecovery_path)
+    guardian_path = os.path.join(drive, "Program Files", "Guardian")
     Log.info('初始化成功。')
 
     time.sleep(2)
@@ -45,8 +45,10 @@ def main():
   \___|_\__,_/__/__/___/__/_\__,_|_||_\__,_|  \___|\_,_\__,_|_| \__,_|_\__,_|_||_| |_|_\___\__\___/\_/\___|_|  \_, |
                                                                                                                |__/ 
                          
-[ClassIsland Guardian Recovery] 版本：0.20260721.1
-正在进行修复，请勿关闭计算机......
+ClassIsland Guardian Recovery
+版本：0.20260721.1 (Mahiro)
+          
+正在准备系统修复。修复完成后会自动进入系统，请安心等待 ~ 
                 
 ''')
     
@@ -54,9 +56,17 @@ def main():
 
     if(os.path.exists(os.path.join(guardianrecovery_path,'Guardian'))):
         try:
+            # 备份日志文件
+            try:
+                shutil.copy2(os.path.join(guardian_path,'guardian.log'), os.path.join(guardianrecovery_path,'guardian.log'))
+                Log.info(f'备份日志文件成功。')
+            except Exception as e:
+                Log.warn(f'备份日志文件失败，错误为：{e}')
+
+            # 修复程序文件
             try:
                 shutil.rmtree(guardian_path)
-                Log.info(f'成功移除了Guardian目录。')
+                Log.info('清除旧程序文件成功。')
             except:
                 pass
             def copy_and_log(src, dst):
@@ -67,7 +77,25 @@ def main():
                 guardian_path,
                 copy_function=copy_and_log
             )
-            Log.info(f'修复成功。')
+            Log.info(f'修复文件成功。')
+
+            # 恢复日志文件
+            try:
+                shutil.copy2(os.path.join(guardianrecovery_path,'guardian.log'), os.path.join(guardian_path,'guardian.log'))
+                os.remove(os.path.join(guardianrecovery_path,'guardian.log'))
+                Log.info(f'恢复日志文件成功。')
+            except Exception as e:
+                Log.warn(f'恢复日志文件失败，错误为：{e}')
+
+            # 为check.exe添加开机自启项
+            startup_dir = os.path.join(drive, "ProgramData", "Microsoft", "Windows", "Start Menu", "Programs", "StartUp")
+            os.makedirs(startup_dir, exist_ok=True)
+            with open(os.path.join(startup_dir, "Guardian_Check.bat"), "w") as f:
+                f.write("""@echo off
+            start /b "" "%ProgramFiles%\\Guardian\\check.exe"
+            """)
+            Log.info('创建check.exe开机自启项成功。')
+
         except Exception as e:
             Log.error(f'修复失败，错误为：{e}')
 
